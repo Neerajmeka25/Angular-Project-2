905ec987b48758119ae68d405fc094e87047cb73
neerajmeka25 <neeraj2512002@gmail.com>
2024-10-22 18:03:56 +0530

    neeraj kau

--- src/app/dashboard/form/form.component.ts
+++ src/app/dashboard/form/form.component.ts
@@ -17,21 +17,27 @@ export class FormComponent implements OnInit {
   enteredEmail: any;
   showVehicleList: boolean = false;
   selectedVehicles: string[] = [];
-  
+  selectedCheckbox: string[] = [];
+
   imageUrl = '../../assets/information.png';
   searchImageUrl = 'assets/search.png';
   filteredVehicles: any[] = [];
   selectedBranch: string = 'All Vehicles';
+  
+  count_step:number = 0;
+  total_step = 1;
+  checkedItem = [
+    { name: "Fleet Wise Report", formName: "fleet", vehicle: false },
+    { name: "Vehicle Wise Report", formName: "vehicle", vehicle: false },
+    { name: "Trip Wise", formName: "trip", vehicle: false },
+    { name: "Driving Scorecard Report", formName: "driving", vehicle: true }
+  ]
 
   constructor(private fb: FormBuilder, private vehicleService: VehicleService) {
     this.vehicleService.getVehicleData().subscribe((data) => {
       this.vehicleList = data.vehicles;
       this.filteredVehicles = this.vehicleList;
-      console.log(this.vehicleList);
     })
-
-
-
   }
 
   ngOnInit(): void {
@@ -56,10 +62,20 @@ export class FormComponent implements OnInit {
     });
   }
 
+  onCheckedBox(name: string){
+    const index = this.selectedCheckbox.indexOf(name);
+    if (index > -1) {
+        this.selectedCheckbox.splice(index, 1);
+    } else {
+        this.selectedCheckbox.push(name);
+    }
+  }
   showInput() {
     this.showEmail = !this.showEmail;
     this.ButtonValue = this.showEmail ? 'Cancel' : '+ Add';
   }
+
+
   filteredVehicleList() {
     const vehicleSearch = this.ScheduleReport.get('vehicleSearch')?.value.toLowerCase();  
     const branch = this.selectedBranch;
@@ -68,8 +84,9 @@ export class FormComponent implements OnInit {
       const matchesSearch = !vehicleSearch || vehicle.registration_number.toLowerCase().includes(vehicleSearch);
       return matchesBranch && matchesSearch;
     });
-
   }
+
+  
   putEmail() {
     let enteredEmail = this.ScheduleReport.get('userEmail');
     if (enteredEmail && enteredEmail.valid && this.emailList.length < 5) {
@@ -91,20 +108,78 @@ export class FormComponent implements OnInit {
   }
 
   onVehicleSelect(vehicle: any) {
-    vehicle.selected = !vehicle.selected;
-    if (vehicle.selected) {
-      this.selectedVehicles.push(vehicle.vin);
+    const index = this.selectedVehicles.indexOf(vehicle.vin);
+    if (index === -1) {
+        this.selectedVehicles.push(vehicle.vin);
     } else {
-      this.selectedVehicles = this.selectedVehicles.filter(vin => vin !== vehicle.vin);
+        this.selectedVehicles.splice(index, 1);
     }
     console.log('Selected Vehicles:', this.selectedVehicles);
   }
 
-  // checkedItem = [
-  //   { name: "Fleet Wise Report", formName: "fleet", vehicle: false },
-  //   { name: "Vehicle Wise Report", formName: "vehicle", vehicle: true },
-  //   { name: "Trip Wise", formName: "trip", vehicle: false },
-  //   { name: "Driving Scorecard Report", formName: "driving", vehicle: true }
-  // ]
+  nextStep(){
+    if(this.count_step<this.total_step){
+      console.log('Selected Vehicles before next step:', this.selectedVehicles);
+      this.count_step++;
+    }
+    else{
+      alert("Last Page")
+    }
+  }
+
+  prevStep(){
+    if(this.count_step==1){
+      this.count_step--;
+    }
+  }
+  hours: number = 12; 
+  minutes: number = 0; 
+  ampm: string = 'AM';
+
+  increment() {
+    if (this.minutes === 59) {
+      this.minutes = 0;
+      if (this.hours === 12) {
+        this.hours = 1;
+      } else {
+        this.hours++;
+      }
+    } else {
+      this.minutes++;
+    }
+  }
+
+  decrement() {
+    if (this.minutes === 0) {
+      this.minutes = 59;
+      if (this.hours === 1) {
+        this.hours = 12;
+      } else {
+        this.hours--;
+      }
+    } else {
+      this.minutes--;
+    }
+  }
+
+
+  toggleAmPm() {
+    this.ampm = this.ampm === 'AM' ? 'PM' : 'AM';
+  }
+
+  formatTime(value: number): string {
+    return value < 10 ? '0' + value : value.toString();
+  }
+  
+
+  days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+  cases = ['Weekly','Every 2 Weeks','Monthly','Quaterly','Yearly'];
+  value : any;
+  selectedOption!: number;
+  onChange(e: any){
+    this.value = e.target.value;
+    console.log(this.value);
+    this.selectedOption = this.cases.indexOf(this.value);
+  }
 
 }
